df9f59c214f492805579b155f8f743466e271347
AurelioMarquesVulcao <aurelio.vulcao@meta.com.br>
2023-02-23 22:51:11 -0300

    feat: mehoria na extração

--- packages/manga/src/extrator/index.ts
+++ packages/manga/src/extrator/index.ts
@@ -1,191 +1,117 @@
-import {
-  conectMongo,
-  convertImageFolderPdf,
-  downloadSoftUrl,
-  Logger,
-  processingSave,
-} from "@extrator/core";
-import axios from "axios";
-import cheerioModule from "cheerio";
-import sleep from "await-sleep";
-import Hash from "object-hash";
-import { disconnectMongo } from "@extrator/core";
-import { extractionSave } from "@extrator/core";
+import { conectMongo, convertImageFolderPdf, downloadSoftUrl, Logger, processingSave } from '@extrator/core'
+import axios from 'axios'
+import cheerioModule from 'cheerio'
+import sleep from 'await-sleep'
+import Hash from 'object-hash'
+import { disconnectMongo } from '@extrator/core'
+import { extractionSave } from '@extrator/core'
+import { resolve } from 'path'
 
-// Problema na variavé de parse
-var parseImages;
-
-var capitulo;
-var capitulos;
-var urlPage = [];
+// // Problema na variavél urlPage, esta com problemas devido a tipagem
+var urlPage = []
 
 export class ExtractManga {
-  logger: any;
-  dataProcessingId: string;
-  processingId: string;
+  logger: any
+  dataProcessingId: string
+  processingId: string
+  parseImages: string
+  capitulo: number
+  capitulos: number
+  path: string
   constructor() {
-    conectMongo();
-    this.dataProcessingId;
-    this.processingId;
+    conectMongo()
+    this.dataProcessingId
+    this.processingId
+    this.parseImages
+    this.capitulo
+    this.capitulos
+    this.path = resolve(__dirname, '../../../../')
   }
-  async extractManga(
-    url: string,
-    manga: string,
-    parse: string,
-    parseButton: string,
-    caps: any
-  ) {
-    const save = await processingSave({ url, manga, parse, parseButton, caps });
-    this.dataProcessingId = save.dataProcessingId;
-    const logger = new Logger("Extrator Manga", this.dataProcessingId);
+  async extractManga(url: string, manga: string, parse: string, parseButton: string, caps: any) {
+    const save = await processingSave({ url, manga, parse, parseButton, caps })
+    this.dataProcessingId = save.dataProcessingId
+    this.parseImages = parse
+    this.capitulo = caps[0]
+    this.capitulos = caps[1]
+    console.log(this.path)
 
-    logger.info(`Extrator Manga: ${manga}`);
+    const logger = new Logger('Extrator Manga', this.dataProcessingId)
 
-    parseImages = parse;
-
-    // console.log(url);
-    // console.log(manga);
-    // console.log(parse);
-    // console.log(paseButton);
-    // console.log(caps);
-    capitulo = caps[0];
-    capitulos = caps[1];
-
-    logger.info("Extracting manga: " + manga);
+    logger.info(`Extrator Manga: ${manga}`)
+    logger.info('Extracting manga: ' + manga)
 
     // start extraction
-    await this.extract(url, manga, parse, parseButton, caps);
-    logger.info("Extracting manga: " + manga + " end!");
-    disconnectMongo();
+    await this.extract(url, manga, parse, parseButton, caps)
+    logger.info('Extracting manga: ' + manga + ' end!')
+    disconnectMongo()
   }
 
-  async extract(
-    url: string,
-    manga: string,
-    parse: string,
-    parseButton: string,
-    caps: any
-  ) {
+  async extract(url: string, manga: string, parse: string, parseButton: string, caps: any) {
     // criando identificador child
-    this.processingId = Hash({
-      url,
-      manga,
-      parse,
-      parseButton,
-      caps,
-      date: new Date(),
-    });
+    this.processingId = Hash({ url, manga, parse, parseButton, caps, date: new Date() })
+    const extract = { url, manga, parse, parseButton, caps, date: new Date() }
 
-    console.log({ url, manga, parse, parseButton, caps, date: new Date() });
+    await extractionSave(this.dataProcessingId, this.processingId, extract, 'started')
 
-    await extractionSave(
-      this.dataProcessingId,
-      this.processingId,
-      {
-        url,
-        manga,
-        parse,
-        parseButton,
-        caps,
-        date: new Date(),
-      },
-      "started"
-    );
+    const logger = new Logger('Extrator Manga', this.dataProcessingId, this.processingId)
 
-    const logger = new Logger(
-      "Extrator Manga",
-      this.dataProcessingId,
-      this.processingId
-    );
-
-    urlPage = [];
-    const pasta =
-      "/home/aurelio/meta/Extrator/downloads/" + manga + "/" + capitulo;
-    const pastaSainda =
-      "/home/aurelio/meta/Extrator/downloads/" +
-      manga +
-      "/" +
-      capitulo +
-      " - " +
-      manga +
-      ".pdf";
-    // const pasta = "/app/downloads/" + manga + "/" + capitulo;
+    urlPage = []
+    const pasta = `${this.path}/downloads/${manga}/${this.capitulo}`
+    const pastaSainda = `${this.path}/downloads/${manga}/${this.capitulo} - ${manga}.pdf`
+    // '/home/vulcao/aurelio/Extrator/downloads/' + manga + '/' + this.capitulo + ' - ' + manga + '.pdf'
+    // const pasta = "/app/downloads/" + manga + "/" + this.capitulo;
     // const pastaSainda =
-    //   "/app/downloads/" + manga + "/" + capitulo + " - " + manga + ".pdf";
-    const extensionFile = ".jpg";
+    //   "/app/downloads/" + manga + "/" + this.capitulo + " - " + manga + ".pdf";
+    const extensionFile = '.jpg'
 
-    logger.info("download iniciado");
-    await downloadSoftUrl(
-      await this.parsePages(url, manga, parse, parseButton, caps),
-      pasta,
-      manga,
-      extensionFile
-    );
+    logger.info('download iniciado')
+    await downloadSoftUrl(await this.parsePages(url, manga, parse, parseButton, caps), pasta, manga, extensionFile)
 
-    await sleep(5000);
-    logger.info("download finalizado");
-    logger.info("conversão em pdf iniciada");
-    await convertImageFolderPdf(pasta, pastaSainda);
-    logger.info("Extracting cap: " + capitulo + " end!");
+    await sleep(5000)
+    logger.info('download finalizado')
+    logger.info('conversão em pdf iniciada')
+    await convertImageFolderPdf(pasta, pastaSainda)
+    logger.info('Extracting cap: ' + this.capitulo + ' end!')
 
-    capitulo++;
-    if (capitulo < capitulos + 1) {
-      await sleep(5000);
-      logger.info("Nova extração iniciada");
-      console.log(urlPage[urlPage.length - 1],
-        manga,
-        parse,
-        parseButton,
-        caps);
-      
-      await this.extract(
-        urlPage[urlPage.length - 1],
-        manga,
-        parse,
-        parseButton,
-        caps
-      );
+    this.capitulo++
+    if (this.capitulo < this.capitulos + 1) {
+      await sleep(5000)
+      logger.info('Nova extração iniciada')
+      console.log(urlPage[urlPage.length - 1], manga, parse, parseButton, caps)
+
+      await this.extract(urlPage[urlPage.length - 1], manga, parse, parseButton, caps)
     }
   }
 
   // extract html from first page
   async extractPage(url: string) {
-    const site = await axios.get(url);
-    const dataSite = site.data;
-    return dataSite;
+    const site = await axios.get(url)
+    const dataSite = site.data
+    return dataSite
   }
 
-  async parsePages(
-    firstPage: string,
-    manga: string,
-    parse: string,
-    paseButton: string,
-    caps: any
-  ) {
-    const body = await this.extractPage(firstPage);
-    const $ = cheerioModule.load(body); //cheerio.load(body)
+  async parsePages(firstPage: string, manga: string, parse: string, paseButton: string, caps: any) {
+    const body = await this.extractPage(firstPage)
+    const $ = cheerioModule.load(body) //cheerio.load(body)
     // para Leitor.net
     $(paseButton).each(async function (element) {
-      let datas = $(this).attr("href");
-      urlPage.push(datas);
-    });
-    // console.log("------------------------------");
-    // console.log(urlPage);
-    // console.log("------------------------------");
-    return await this.parseImage(firstPage);
+      let datas = $(this).attr('href')
+      urlPage.push(datas)
+    })
+    return await this.parseImage(firstPage)
   }
 
   async parseImage(page) {
-    let url = [];
-    const body = await this.extractPage(page);
-    const $ = cheerioModule.load(body);
-    $(parseImages).each(async function (element) {
-      let datas = $(this).attr("src");
-      url.push(datas);
-    });
+    let url = []
+    const body = await this.extractPage(page)
+    const $ = cheerioModule.load(body)
+    $(this.parseImages).each(async function (element) {
+      let datas = $(this).attr('src')
+      url.push(datas)
+    })
     // REMOVE URL DESNECESSARIAS
-    url.shift();
+    url.shift()
     // console.log(url);
-    return url;
+    return url
   }
 }
--- packages/manga/src/index.ts
+++ packages/manga/src/index.ts
@@ -1,30 +1,34 @@
-import express from "express";
-import cors from "cors";
-import Routes from "./routes";
+import express from 'express'
+import cors from 'cors'
+import Routes from './routes'
+import cluster from 'cluster'
+import { resolve } from 'path'
+import dotenv from 'dotenv'
 
-const cluster = require("cluster");
 // const numCPUs = require('os').cpus().length;
-const numCPUs = 1;
-console.log(numCPUs);
+const numCPUs = 1
+console.log(numCPUs)
+
+
 
 // import {routes} from "routes"
 // const cors = require("cors");
 // const express = require("express");
 
-const port = "3300";
+const port = process.env.PORT || 3000
+
 // const this.express = express()
 class App {
-  public express: express.Application;
+  public express: express.Application
   public constructor() {
-    this.express = express();
+    this.express = express()
     // this.database();
-    this.middlewares();
-    this.routes();
+    this.middlewares()
+    this.routes()
 
-    this.multi();
+    this.multi()
 
-    console.log(new Date());
-    
+    console.log(new Date())
 
     // this.express.listen(port, () =>
     //   console.log(`Sua API REST está funcionando na porta ${port} `)
@@ -33,31 +37,31 @@ class App {
   }
 
   middlewares() {
-    this.express.use(express.json());
-    this.express.use(cors());
+    this.express.use(express.json())
+    this.express.use(cors())
   }
 
   routes() {
     // this.express.use(routes);
-    this.express.use("", Routes.router);
+    this.express.use('', Routes.router)
   }
 
   /*
-  Abre em multi processamento para não bagunçar a geração de arquivo 
+  Abre em multi processamento para não bagunçar a geração de arquivo
   */
   multi() {
     // For Master process
     if (cluster.isMaster) {
-      console.log(`Master ${process.pid} is running`);
+      console.log(`Master ${process.pid} is running`)
 
       // Fork workers.
       for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
+        cluster.fork()
       }
       // This event is firs when worker died
-      cluster.on("exit", (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-      });
+      cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`)
+      })
     }
 
     // For Worker
@@ -65,11 +69,9 @@ class App {
       // Workers can share any TCP connection
       // In this case it is an HTTP server
       this.express.listen(port, (err) => {
-        err
-          ? console.log("Error in server setup")
-          : console.log(`Worker ${process.pid} started`);
-      });
+        err ? console.log('Error in server setup') : console.log(`Worker ${process.pid} started`)
+      })
     }
   }
 }
-module.exports = new App();
+module.exports = new App()
